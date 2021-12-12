452c518c59cf73f71e04406b5d6cee5f352440d3
emilje <emil.jerkovic@gmail.com>
2021-12-12 15:59:16 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "fb7119fd-ea76-42d6-96c5-b4dc37da03a1";
+export const MODAL_CLOSE_SECONDS = 2.5;
